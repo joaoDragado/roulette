d99af53eff5f0f6b586b4cd04a06161cfa44540a
joaoDragado <dubmecoco@yahoo.com>
2017-11-03 12:04:11 +0200

    completed Outcome class + tests

--- outcome.py
+++ outcome.py
@@ -1,17 +1,45 @@
-
-''' Outcome
-
-Responsibilities.
-    A name for the bet and the payout odds. This isolates the
-    calculation of the payout amount. Example: “Red”, “1:1”.
-
-Collaborators.
-    - Collected by Wheel into the bins that reflect the bets that win;
-    - collected by Table into the available bets for the Player;
-    - used by Game to compute the amount won from the amount that was
-    bet.
-'''
     
 class Outcome(object):
-    def __init__(self):
-        pass
+    ''' Outcome
+    Responsibilities.
+        A name for the bet and the payout odds. This isolates the calculation of the payout amount. Example: “Red”, “1”.
+    Collaborators.
+        - Collected by Wheel into the bins that reflect the bets that win;
+        - collected by Table into the available bets for the    Player;
+        - used by Game to compute the amount won from the amount that was bet.
+    '''
+    def __init__(self, name, odds):
+        self.name = name
+        self.odds = odds
+
+
+    def __eq__(self, other):
+        '''Test Equality of the name attributes of self and other.'''
+        return self.name == other.name
+
+
+    def __ne__(self, other):
+        '''Test Inequality of the name attributes of self and other.'''
+        return self.name != other.name 
+
+
+    def __hash__(self):
+        '''Hash value for this outcome.'''
+        return hash(self.name)
+
+
+    def __str__(self):
+        '''Easy-to-Read representation of outcome'''
+        return '{name:s} ({odds:d}:1)'.format_map(vars(self))
+
+
+    def __repr__(self):
+        '''Detailed representation of this outcome.'''
+        return '<Outcome "{name:s}" {odds:d}:1>'.format_map(vars(self))
+
+    
+    def winAmount(self, amount):
+        '''Multiply this Outcome‘s odds by the given amount.
+        The product is returned.'''
+        return self.odds * amount
+
--- test/test_outcome.py
+++ test/test_outcome.py
@@ -1,1 +1,22 @@
+import pytest
+from ..outcome import Outcome
 
+o1 = Outcome("Red", 1)
+o2 = Outcome("Red", 2)
+o3 = Outcome("18", 35)
+
+
+def test_outcome_eq():
+    assert o1 == o2
+
+
+def test_outcome_ne():
+    assert o1 != o3
+
+
+def test_hash():
+    assert hash(o1) == hash(o2)
+
+
+def test_winAmount():
+    assert o3.winAmount(20) == 700
