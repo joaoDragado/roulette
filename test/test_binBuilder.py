af55560adce1b0e62d51f0bc072b8a9018f5a99a
joaoDragado <dubmecoco@yahoo.com>
2017-11-08 22:52:14 +0200

    skeleton binBuilder tests

---
+++ test/test_binBuilder.py
@@ -0,0 +1,32 @@
+import pytest
+from ..binBuilder import binBuilder
+    
+def test_buildBins():
+    pass
+
+def test_gen_straight_bets():
+    pass
+
+def test_gen_split_bets():
+    pass
+
+def test_gen_street_bets():
+    pass
+
+def test_gen_corner_bets():
+    pass
+
+def test_gen_line_bets():
+    pass
+
+def test_gen_dozen_bets():
+    pass
+
+def test_gen_column_bets():
+    pass
+
+def test_gen_even_money_bets():
+    pass
+
+def test_gen_zeros_bets():
+    pass
