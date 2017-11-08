d1d9c9821174582eff412fc05def35a7467fb830
joaoDragado <dubmecoco@yahoo.com>
2017-11-08 18:34:42 +0200

    completed Wheel class + tests

--- test/test_wheel.py
+++ test/test_wheel.py
@@ -1,16 +1,17 @@
 import pytest
-import random
 from ..outcome import Outcome
+from ..bin import Bin
 from ..wheel import Wheel
+from ..non_rnd import Non_rnd
 
 # outcomes
 o0 = Outcome('0', 35)
 o37 = Outcome('00', 35)
 o5 = Outcome('Five Bet', 6)
-# wheel
-wheel = Wheel()
 
 def test_addOutcomes():
+    # wheel
+    wheel = Wheel()
     # add outcomes to wheel
     wheel.addOutcome(0,o0)
     wheel.addOutcome(0,o5)
@@ -23,4 +24,15 @@ def test_addOutcomes():
     assert len(wheel.bins[37]) == 2
 
 
-    
+def test_wheel_spin():
+    wheel = Wheel(rng=Non_rnd([0,37]))
+    # add outcomes to wheel
+    wheel.addOutcome(0,o0)
+    wheel.addOutcome(0,o5)
+    wheel.addOutcome(37,o37)
+    wheel.addOutcome(37,o5)
+    b0 = frozenset([o0, o5])
+    b37 = frozenset([o37, o5])
+    assert wheel.next() == b0
+    assert wheel.next() == b37
+      
