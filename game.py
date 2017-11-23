2aa09d355a3b917c73f80b3c3c140ee61fd4592f
joaoDragado <dubmecoco@yahoo.com>
2017-11-23 18:19:17 +0200

    built skeleton Player & Game classes

--- game.py
+++ game.py
@@ -1,14 +1,23 @@
 
-''' Game
 
-Responsibilities.
-    Runs the game: gets bets from Player, spins Wheel, collects losing bets, pays winning bets.
-    This encapsulates the basic sequence of play into a single class.
-
-Collaborators.
-    Uses Wheel, Table, Outcome, Player. The overall statistical analysis will play a finite number of games and collect the final value of the Player‘s stake.
-'''
 
 class Game(object):
-    def __init__(self):
+    ''' Game manages the sequence of actions that defines the game of Roulette. 
+    Game notifies Player to place Bets, spins Wheel, & resolves the Bets present on the Table.
+
+    The final aim is to use this class to run simulations on betting strategies , play a finite number of games, and collect the final value of the Player‘s stake.
+    '''
+    def __init__(self, wheel, table):
+        '''Constructs a new Game, using a given Wheel and Table.
+        We utilize the Strategy design pattern ; 
+        Each of these collaborating objects is  a replaceable strategy, and can be changed by the client that uses this game.'''
+        pass
+    
+    def cycle(self, player):
+        ''' 
+        Executes a single cycle of play with a given Player. 
+        1. call thePlayer‘s placeBets() method to get bets,
+        2. call theWheel‘s next() method to get the next winning Bin, 
+        3. call theTable‘s iterator to get an Iterator over the Bets. If the winning Bin contains the Outcome, call the thePlayer win() method otherwise call the thePlayer lose() method.
+        '''
         pass
--- player.py
+++ player.py
@@ -1,12 +1,35 @@
-''' Player
-
-Responsibilities.
-    Places bets on Outcomes, updates the stake with amounts won and lost.
-
-Collaborators.
-    Uses Table to place bets on Outcomes; used by Game to record wins and losses.
-'''
-
+# this will act as a superclass to individual player types.
 class Player(object):
+    ''' Player Class
+    Places bets on Outcomes, updates the stake with amounts won and lost.
+    Uses Table to place bets on Outcomes; used by Game to record wins and losses.
+    '''
     def __init__(self):
+        pass
+
+
+class Passenger57(object):
+    '''Passenger57 constructs a Bet based on the Outcome named "Black". 
+    This is a very persistent player'''
+    
+    def __init__(self, table, wheel):
+        '''Constructs the Player with a specific table for placing bets. 
+        This also creates the “black” Outcome. 
+        This is saved in a variable named Passenger57.black for use in creating bets.
+        '''
+        pass
+    
+    def placeBets(self):
+        '''Updates the Table with the various bets. This version creates a Bet instance from the black Outcome. 
+        It uses Table placeBet() to place that bet.'''
+        pass
+    
+    def win(self, bet):
+        '''Notification from the Game that the Bet was a winner. 
+        The amount of money won is available via a the
+        winAmount() method of theBet.'''
+        pass
+    
+    def lose(self, bet):
+        '''Notification from the Game that the Bet was a loser.'''
         pass
